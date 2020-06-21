326fc736eebe4f7518bda580826cf9a1f0c19105
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:31:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números");
+            Console.Write("Primeiro número: ");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Segundo número: ");
+            int n2=Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            
+            if(n1>n2)
+            Console.WriteLine("O maior é " +n1);
+            else
+            Console.WriteLine("O maior é " +n2);
+            }
+
+
+
+        }
+    }
+
